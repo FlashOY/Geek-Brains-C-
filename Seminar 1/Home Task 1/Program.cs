fe6340975a775c17ff04055e2df042183b3e797a
Flash <flash@example.com>
2023-07-05 18:57:37 +0300

    My home work 1

---
+++ Seminar 1/Home Task 1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+ Console.Write("Enter the number a:");
+
+ int a = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the number b:");
+
+int b = int.Parse(Console.ReadLine());
+
+int max =0;
+
+
+if (a > b) {
+    max = a;
+    Console.WriteLine("a biggest then b!");
+} else {  max = b;
+Console.WriteLine("b biggest then a!");
+}
+
