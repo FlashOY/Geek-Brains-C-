0b7f9e3372680f6007b728ba8044ac5b75253267
Flash <flash@example.com>
2023-07-06 18:24:06 +0300

    Seminar task 4

---
+++ Seminar 2/Seminar task 4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+ Console.WriteLine("Enter the number a");
+int a = int.Parse(Console.ReadLine());
+
+if ((a % 7 == 0) && (a % 23 == 0)) {
+    Console.WriteLine("Dev");
+} else {
+    Console.WriteLine("No");
+}
