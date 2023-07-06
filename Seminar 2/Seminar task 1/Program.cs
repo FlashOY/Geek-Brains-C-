0deaacc294869acd6d91e54387ee7f95f364100f
Flash <flash@example.com>
2023-07-06 12:33:34 +0300

    add Seminar task 1

---
+++ Seminar 2/Seminar task 1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter the number: ");
+int number = int.Parse(Console.ReadLine());
+ int index = 1; 
+
+while (index <= number) {
+    Console.WriteLine(index);
+    index= index +1; 
+}
