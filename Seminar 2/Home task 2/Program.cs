f5fc6c0bc80a10d6f79abd7a94a51c0ee2f97c6f
Flash <flash@example.com>
2023-07-06 20:05:40 +0300

    Seminar 2Home task 3

---
+++ Seminar 2/Home task 2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+
+Console.WriteLine("Enter the number: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 100 ) 
+{Console.WriteLine("Incorrect number");
+}
+
+
+int temp = Math.Abs(number);
+while (temp >= 1000 ){
+    temp = temp / 10; 
+} 
+
+int result = temp % 10; 
+Console.WriteLine($"Third number {result}");
