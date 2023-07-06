a9de39d43e81056e4b3ee57b18ff95f91b25341c
Flash <flash@example.com>
2023-07-06 18:22:23 +0300

    Seminar task 1

---
+++ Seminar 2/Seminar task 2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+int randomNumber = new Random().Next(100, 1000);
+Console.WriteLine(randomNumber);
+
+//int a = randomNumber /100; 
+//int b = randomNumber %10;
+//Console.WriteLine(a);
+//Console.WriteLine(b);
+
+int result = ((randomNumber/100)*10)+(randomNumber % 10);
+Console.WriteLine(result);
