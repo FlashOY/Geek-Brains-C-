a68173af177a5396bd0d6df5bcddb4b5970e74f6
Flash <flash@example.com>
2023-07-06 19:54:22 +0300

    Seminar 2 Home task 2

---
+++ Seminar 2/Home task 3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter a number (1-7) representing the day of the week: ");
+        int dayOfWeek = int.Parse(Console.ReadLine());
+
+        bool isWeekend = (dayOfWeek == 6 || dayOfWeek == 7);
+
+        if (isWeekend) {
+        Console.WriteLine("Yes, it's a weekend day.");}
+        else
+        {Console.WriteLine("No, it's not a weekend day."); }
