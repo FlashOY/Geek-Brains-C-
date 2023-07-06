43023d5d3b30325168deaf888ce72b046fdc78b0
Flash <flash@example.com>
2023-07-06 18:17:30 +0300

    Home work 1

---
+++ Seminar 2/Home task 1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Write("Enter the number: ");
+int number = int.Parse(Console.ReadLine());
+
+if ((number <=999) && (number >= 100)) {
+    Console.WriteLine($"Number is {(number/10)%10 }");} 
+    else{
+        Console.WriteLine("Incorrect number");
+        }
