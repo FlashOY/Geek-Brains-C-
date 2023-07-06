5efcd85ea33344e0e99855ea26ab148eddce2938
Flash <flash@example.com>
2023-07-06 18:24:52 +0300

    Seminar task 6

---
+++ Seminar 2/Seminar task 6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите процграмму, которая примет на вход число от пользователя и выведет на экран кол-во цифр в этом числе. Число может быть любой разрядности. 
+// 43 is 2 
+Console.WriteLine ("Enter the number");
+int a = int.Parse(Console.ReadLine()); 
+int count = 1; 
+while (a/10 !=0) {
+    a = a/10; 
+    count++;
+}
+
+Console.WriteLine ($"count is {count}");
