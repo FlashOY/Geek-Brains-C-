97d624ffe560a8534ab38ff13bb3ffa11f1c7e9a
Flash <flash@example.com>
2023-07-06 18:24:25 +0300

    Seminar task 5

---
+++ Seminar 2/Seminar task 5/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+ 
+Console.WriteLine("Enter the number a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the number b");
+int b = int.Parse(Console.ReadLine());
+
+// b = a/b - тоже можно как вариант 
+if (b == (a/b)) {
+    Console.WriteLine("Yes");
+} else {
+    Console.WriteLine("No");
+}
+
+if ((a * a == b)||(b * b == a)) {
+    Console.WriteLine("Yes");
+} else {
+    Console.WriteLine("No");
+}
