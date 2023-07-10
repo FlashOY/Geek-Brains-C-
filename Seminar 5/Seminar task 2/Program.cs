4b488b82fb53c1a31f684b7c2236a0288e9b2e74
Flash <flash@example.com>
2023-07-10 15:22:47 +0300

    +

---
+++ Seminar 5/Seminar task 2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+int [] a= {-4, -8, 8, 2};
+ 
+
+int [] sort  (int [] array) {
+
+    for (int i = 0; i < array.Length; i++) 
+    {
+           array[i] = - array[i];
+    }
+
+ 
+   return   array; 
+}
+ 
+int [] result  = sort(a); 
+System.Console.WriteLine($"Result: {string.Join(", ", result)}");
